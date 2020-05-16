f49b5dff2148bb9e66ca53d3f53c8557d994b9f6
Sanjay <info@sanjaybharti.com>
2020-05-16 15:27:34 +0200

    Final

--- run.py
+++ run.py
@@ -1,14 +1,9 @@
-
-import time
-import random
 import string
 from src.filter import Finder
 
 if __name__ == '__main__':
     string_list = ["asd", "asdd", "fre", "glk", "lkm", "stdo"]
-    t1 = time.time()
+    
     finder = Finder(string_list)
     print(finder.find('sad'))
-    t2 = time.time()
 
-    print('time: {}'.format(t2-t1))
